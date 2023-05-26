a1bd65f6ff80c2db9b30345b1d00d39cb64c82bf
Kyle Chan <kachan@pldt.com.ph>
2023-05-27 00:11:42 +0800

    Add multiprocessing pool

--- text_extract.py
+++ text_extract.py
@@ -1,6 +1,6 @@
 from utils import read_progress, write_header_file_ref,  get_all_filepaths, extract_text
 from tqdm import tqdm
-from multiprocessing import pool
+from multiprocessing import Pool
 # DONE
 # SHA1
 # PROGRESS INDICATOR (2 LISTS DONE AND NOT DONE)
@@ -9,24 +9,35 @@ from multiprocessing import pool
 
 # TO DO
 # MULTIPROCESSING
+progress = read_progress()
+
+def run(path):
+    if path.full_path in progress:
+        print('here', flush=True)
+        pass
+    else:
+        extract_text(path) 
 
 
 def main():
 
-    progress = read_progress()
     write_header_file_ref()
 
     paths_list = get_all_filepaths()
     len_paths = len(paths_list)
 
-    for i in tqdm(range(len_paths)):
-        path = paths_list[i]
-        if path.full_path in progress:
-            print('here')
-            pass
-        else:
-            extract_text(path)
+    # for i in tqdm(range(len_paths)):
+    #     path = paths_list[i]
+    #     if path.full_path in progress:
+    #         print('here')
+    #         pass
+    #     else:
+    #         extract_text(path)
 
+    p = Pool()
+
+    for _ in tqdm(p.imap_unordered(run, paths_list), total=len_paths):
+        pass
 
 
 if __name__ == '__main__':
